c0279241041dba575c8a11bcf83d3641c8eafb37
Evelyn Monteros <Monterosvaca@gmail.com>
2025-02-08 15:43:38 -0500

    new

--- csce242/assignments/assignment05/script.js
+++ csce242/assignments/assignment05/script.js
@@ -6,7 +6,12 @@
 
 
 
- 
+ //change color
+
+
+
+
+
 
 
  //image change
@@ -15,11 +20,3 @@
     document.getElementById("img").src = "images/UofSC.png"; 
 }
 
-
-
-const colorPicker = document.getElementById('colorPicker');
-const star = document.getElementById('star');
-
-colorPicker.addEventListener('input', function(event) {
-  star.style.fill = event.target.value;
-});
